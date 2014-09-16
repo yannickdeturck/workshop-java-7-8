17d666a50f2e70c99e7be64ab9e1d9aed172f6a6
Ken Coenen <ken.coenen@hp.com>
2014-09-16 17:27:52 +0200

    Added DarteTime exercise

--- exercises/src/main/java/be/ordina/workshop/java8/exercises/Exercise1.java
+++ exercises/src/main/java/be/ordina/workshop/java8/exercises/Exercise1.java
@@ -1,41 +1,96 @@
 package be.ordina.workshop.java8.exercises;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+  import java.time.LocalDate;
+  import java.time.Period;
+  import java.util.ArrayList;
+  import java.util.Arrays;
+  import java.util.List;
+  import java.util.stream.Collectors;
 
 /**
  * Basic exercises on the new features in Java 8.
  *
  * @author Yannick De Turck
+ * @author Ken Coenen
  */
 public class Exercise1 {
-    private static List<String> fruits = Arrays.asList("apple", "pear", "banana", "orange", "pineapple", "lemon");
+	private static List<Food> fridgeContent = Arrays.asList(
+	  new Food("apple", Category.FRUITS, LocalDate.now().plus(Period.ofWeeks(1))),
+	  new Food("pear", Category.FRUITS, LocalDate.now().plus(Period.ofDays(5))),
+	  new Food("orange", Category.FRUITS, LocalDate.now().plus(Period.ofWeeks(1))),
+	  new Food("pineapple", Category.FRUITS, LocalDate.now().plus(Period.ofWeeks(1))),
+	  new Food("pizza", Category.FREEZER, LocalDate.now().plus(Period.ofWeeks(8))),
+	  new Food("lettuce", Category.VEGETABLES, LocalDate.now().plus(Period.ofDays(5))),
+	  new Food("carrot", Category.VEGETABLES, LocalDate.now().plus(Period.ofDays(10))),
+	  new Food("steak", Category.MEAT, LocalDate.now().plus(Period.ofDays(2))),
+	  new Food("salmon", Category.FISH, LocalDate.now().plus(Period.ofDays(4))),
+	  new Food("lasagna", Category.PREPARED_MEALS, LocalDate.now().plus(Period.ofWeeks(1))),
+	  new Food("soup", Category.PREPARED_MEALS, LocalDate.now().plus(Period.ofWeeks(1))),
+	  new Food("frozen soup", Category.FREEZER, LocalDate.now().plus(Period.ofWeeks(10)))
+	);
 
-	// TODO implement this method using streams to filter out all fruits with an uneven character size
-	public static List<String> filterEvenCharacterSize() {
+	// TODO implement this method using streams to filter out all food in our fridge with an even character size
+	public static List<Food> filterEvenCharacterSize() {
 		return new ArrayList<>();
 	}
 
-    // TODO implement this method using streams to sort the list alphabetically
-    public static List<String> sortAlphabetically() {
-        return new ArrayList<>();
-    }
+	// TODO implement this method using streams to sort the list alphabetically
+	public static List<Food> sortAlphabetically() {
+		return new ArrayList<>();
+	}
 
-    // TODO implement this method using streams to return a list of the fruits where each fruit is limited to its 3 first characters
-    public static List<String> mapThreeFirstCharacters() {
-        return new ArrayList<>();
-    }
+	// TODO implement this method using streams to return a list of food names from our fridge where each name is limited to its 3 first characters
+	public static List<String> mapThreeFirstCharacters() {
+		return new ArrayList<>();
+	}
 
-    // TODO implement this method using streams and return a boolean reflecting whether a fruit's size matches with the given size
-    public static boolean fruitWithSizeExists(int size) {
-        return false;
-    }
+	// TODO implement this method using streams and return a boolean reflecting whether a food name's size matches with the given size
+	public static boolean foodWithSizeExists(int size) {
+		return false;
+	}
 
-    // TODO implement this method using the new forEach-method available to all Collections to join all fruits in a single String utilising a StringBuilder
-    public static String toSingleString() {
-        StringBuilder bob = new StringBuilder();
-        // TODO add forEach here!
-        return bob.toString();
-    }
+	// TODO implement this method using the new forEach-method available to all Collections to join all food in our fridge in a single String utilising a StringBuilder
+	public static String toSingleString() {
+		StringBuilder bob = new StringBuilder();
+		// TODO add forEach here!
+		return bob.toString();
+	}
+
+	// TODO implement this method using streams and the new DateTime API to return all expired food after the given day
+	public static List<Food> getExpiredFood(int day) {
+		return new ArrayList<>();
+	}
+
+	public static class Food {
+		private String name;
+		private Category category;
+		private LocalDate expirationDate;
+
+		private Food(String name, Category category, LocalDate expirationDate) {
+			this.name = name;
+			this.category = category;
+			this.expirationDate = expirationDate;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Category getCategory() {
+			return category;
+		}
+
+		public LocalDate getExpirationDate() {
+			return expirationDate;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+
+	enum Category {
+		FRUITS, VEGETABLES, MEAT, FISH, PREPARED_MEALS, FREEZER;
+	}
 }
--- exercises/src/test/java/be/ordina/workshop/java8/exercises/tweeter/Exercise1Test.java
+++ exercises/src/test/java/be/ordina/workshop/java8/exercises/tweeter/Exercise1Test.java
@@ -10,50 +10,74 @@ import java.util.List;
 
 /**
  * @author Yannick De Turck
+ * @author Ken Coenen
  */
 @RunWith(JUnit4.class)
 public class Exercise1Test {
-    @Test
-    public void testFilterUnevenCharacterSize() {
-        List<String> unevenFruits = Exercise1.filterEvenCharacterSize();
-        Assert.assertEquals(3, unevenFruits.size());
-        Assert.assertTrue(unevenFruits.contains("pear"));
-        Assert.assertTrue(unevenFruits.contains("banana"));
-        Assert.assertTrue(unevenFruits.contains("orange"));
-    }
+	@Test
+	public void testFilterEvenCharacterSize() {
+		List<Exercise1.Food> evenFood = Exercise1.filterEvenCharacterSize();
+		Assert.assertEquals(6, evenFood.size());
+	}
 
-    @Test
-    public void testSortAlphabetically() {
-        List<String> sortedFruits = Exercise1.sortAlphabetically();
-        Assert.assertEquals("apple", sortedFruits.get(0));
-        Assert.assertEquals("banana", sortedFruits.get(1));
-        Assert.assertEquals("lemon", sortedFruits.get(2));
-        Assert.assertEquals("orange", sortedFruits.get(3));
-        Assert.assertEquals("pear", sortedFruits.get(4));
-        Assert.assertEquals("pineapple", sortedFruits.get(5));
-    }
+	@Test
+	public void testSortAlphabetically() {
+		List<Exercise1.Food> sortedFruits = Exercise1.sortAlphabetically();
+		int i = 0;
+		Assert.assertEquals("apple", sortedFruits.get(i++).getName());
+		Assert.assertEquals("carrot", sortedFruits.get(i++).getName());
+		Assert.assertEquals("frozen soup", sortedFruits.get(i++).getName());
+		Assert.assertEquals("lasagna", sortedFruits.get(i++).getName());
+		Assert.assertEquals("lettuce", sortedFruits.get(i++).getName());
+		Assert.assertEquals("orange", sortedFruits.get(i++).getName());
+		Assert.assertEquals("pear", sortedFruits.get(i++).getName());
+		Assert.assertEquals("pineapple", sortedFruits.get(i++).getName());
+		Assert.assertEquals("pizza", sortedFruits.get(i++).getName());
+		Assert.assertEquals("salmon", sortedFruits.get(i++).getName());
+		Assert.assertEquals("soup", sortedFruits.get(i++).getName());
+		Assert.assertEquals("steak", sortedFruits.get(i++).getName());
+	}
 
-    @Test
-    public void testMapThreeFirstCharacters() {
-        List<String> fruits = Exercise1.mapThreeFirstCharacters();
-        Assert.assertEquals("app", fruits.get(0));
-        Assert.assertEquals("pea", fruits.get(1));
-        Assert.assertEquals("ban", fruits.get(2));
-        Assert.assertEquals("ora", fruits.get(3));
-        Assert.assertEquals("pin", fruits.get(4));
-        Assert.assertEquals("lem", fruits.get(5));
-    }
+	@Test
+	public void testMapThreeFirstCharacters() {
+		List<String> food = Exercise1.mapThreeFirstCharacters();
+		int i = 0;
+		Assert.assertEquals("app", food.get(i++));
+		Assert.assertEquals("pea", food.get(i++));
+		Assert.assertEquals("ora", food.get(i++));
+		Assert.assertEquals("pin", food.get(i++));
+		Assert.assertEquals("piz", food.get(i++));
+		Assert.assertEquals("let", food.get(i++));
+		Assert.assertEquals("car", food.get(i++));
+		Assert.assertEquals("ste", food.get(i++));
+		Assert.assertEquals("sal", food.get(i++));
+		Assert.assertEquals("las", food.get(i++));
+		Assert.assertEquals("sou", food.get(i++));
+		Assert.assertEquals("fro", food.get(i++));
+	}
 
-    @Test
-    public void testFruitWithSizeExists() {
-        Assert.assertTrue(Exercise1.fruitWithSizeExists(5));
-        Assert.assertTrue(Exercise1.fruitWithSizeExists(9));
-        Assert.assertFalse(Exercise1.fruitWithSizeExists(2));
-        Assert.assertFalse(Exercise1.fruitWithSizeExists(7));
-    }
+	@Test
+	public void testFruitWithSizeExists() {
+		Assert.assertTrue(Exercise1.foodWithSizeExists(5));
+		Assert.assertTrue(Exercise1.foodWithSizeExists(9));
+		Assert.assertFalse(Exercise1.foodWithSizeExists(2));
+		Assert.assertFalse(Exercise1.foodWithSizeExists(7));
+	}
 
-    @Test
-    public void testToSingleString() {
-        Assert.assertEquals("applepearbananaorangepineapplelemon", Exercise1.toSingleString());
-    }
+	@Test
+	public void testToSingleString() {
+		Assert.assertEquals("applepearorangepineapplepizzalettucecarrotsteaksalmonlasagnasoupfrozen soup", Exercise1.toSingleString());
+	}
+
+	@Test
+	public void testGetExpiredFood() {
+		List<Exercise1.Food> expiredFood = Exercise1.getExpiredFood(3);
+		Assert.assertEquals(1, expiredFood.size());
+
+		expiredFood = Exercise1.getExpiredFood(6);
+		Assert.assertEquals(4, expiredFood.size());
+
+		expiredFood = Exercise1.getExpiredFood(10);
+		Assert.assertEquals(9, expiredFood.size());
+	}
 }
